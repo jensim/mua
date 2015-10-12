872d18c6578ff182acf824829d45bbcf8e34e6d8
jens brimberg <jens.brimberg@gmail.com>
2015-10-12 19:42:07 +0200

    Adding json REST backend to web.js

--- web.js
+++ web.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.get('/data', function (req, res) {
+	res.send([{
+		name: 'wine1'
+	}, {
+		name: 'wine2'
+	}]);
+});
+
 app.use('/', express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
-
