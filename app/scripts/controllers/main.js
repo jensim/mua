b9b1b86907d3558febd0a9ff04aa43218ddbd8f9
jens brimberg <jens.brimberg@gmail.com>
2015-10-12 06:32:27 +0200

    Yeoman Angular without Compass Sass

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ddApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ddApp
+ */
+angular.module('ddApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
