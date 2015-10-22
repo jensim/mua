5807dc0782a383d046c8d26669e2b233e0975e69
jens brimberg <jens.brimberg@gmail.com>
2015-10-22 07:13:22 +0200

    Copying some features from the old chargen project to the mutant static data factory

--- client/app/mutantStaticdataFactory/mutantStaticdataFactory.service.js
+++ client/app/mutantStaticdataFactory/mutantStaticdataFactory.service.js
@@ -1,6 +1,40 @@
 'use strict';
 
-angular.module('dndToolApp').service('mutantStaticdataFactory', function () {
+angular.module('dndToolApp').factory('mutantStaticdataFactory', function ($http, $localStorage, $log) {
 	// AngularJS will instantiate a singleton by calling "new" on this function
+	var dataLoaded = false,
+		staticData,
+		storageM = function (forceReset) {
+			if ($localStorage.storage === undefined || $localStorage.storage.version === undefined || $localStorage.storage.version !== staticData.version || forceReset === true) {
+				$localStorage.storage = {
+					version: staticData.version
+				};
+			}
+		},
+		storageMS = function (forceReset) {
+			if ($localStorage.flatData === undefined || $localStorage.flatData.version === undefined || $localStorage.flatData.version !== staticData.version || forceReset === true) {
+				$http.get('/api/mutantdata').then(
+					function (req, res) {
+						staticData = res.data;
+						$log.debug(res.data);
+					},
+					function (req, res) {
+						$log.error(res.status + ': Server responded poorly..');
+					});
+			}
+		};
 
+	(function () {
+		storageM();
+		storageMS();
+	})();
+
+	return {
+		resetStaticData: function () {
+			storageMS(true);
+		},
+		resetData: function () {
+			storageM(true);
+		}
+	};
 });
