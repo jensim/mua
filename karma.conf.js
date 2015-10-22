9f751fb417684266688469ef046bdcaee576f37e
jens brimberg <jens.brimberg@gmail.com>
2015-10-22 18:42:45 +0200

    Added ngStorage dependency to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,16 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+module.exports = function (config) {
+	config.set({
+		// base path, that will be used to resolve files and exclude
+		basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+		// testing framework to use (jasmine/mocha/qunit/...)
+		frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
+		// list of files / patterns to load in the browser
+		files: [
       'client/bower_components/jquery/dist/jquery.js',
       'client/bower_components/angular/angular.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'client/bower_components/lodash/dist/lodash.compat.js',
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+		'client/bower_components/ngstorage/ngStorage.js',
       'client/app/app.js',
       'client/app/app.coffee',
       'client/app/**/*.js',
@@ -34,63 +35,63 @@ module.exports = function(config) {
       'client/components/**/*.html'
     ],
 
-    preprocessors: {
-      '**/*.jade': 'ng-jade2js',
-      '**/*.html': 'html2js',
-      'client/app/**/*.js': 'babel',
-      '**/*.coffee': 'coffee',
-    },
+		preprocessors: {
+			'**/*.jade': 'ng-jade2js',
+			'**/*.html': 'html2js',
+			'client/app/**/*.js': 'babel',
+			'**/*.coffee': 'coffee',
+		},
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: 'client/'
-    },
+		ngHtml2JsPreprocessor: {
+			stripPrefix: 'client/'
+		},
 
-    ngJade2JsPreprocessor: {
-      stripPrefix: 'client/'
-    },
-
-    
-    babelPreprocessor: {
-      options: {
-        sourceMap: 'inline'
-      },
-      filename: function (file) {
-        return file.originalPath.replace(/\.js$/, '.es5.js');
-      },
-      sourceFileName: function (file) {
-        return file.originalPath;
-      }
-    },
-    
-
-    // list of files / patterns to exclude
-    exclude: [],
-
-    // web server port
-    port: 8080,
-
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		ngJade2JsPreprocessor: {
+			stripPrefix: 'client/'
+		},
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+		babelPreprocessor: {
+			options: {
+				sourceMap: 'inline'
+			},
+			filename: function (file) {
+				return file.originalPath.replace(/\.js$/, '.es5.js');
+			},
+			sourceFileName: function (file) {
+				return file.originalPath;
+			}
+		},
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['PhantomJS'],
+		// list of files / patterns to exclude
+		exclude: [],
+
+		// web server port
+		port: 8080,
+
+		// level of logging
+		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
+
+
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['PhantomJS'],
+
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false
+	});
 };
