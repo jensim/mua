d961a98a87a977ab01db1494222f506194085ea1
jens brimberg <jens.brimberg@gmail.com>
2015-11-01 16:06:59 +0100

    now generating coverage reports on tests

--- karma.conf.js
+++ karma.conf.js
@@ -9,37 +9,60 @@ module.exports = function (config) {
 		// testing framework to use (jasmine/mocha/qunit/...)
 		frameworks: ['jasmine'],
 
+		plugins: [
+			"karma-babel-preprocessor",
+			"karma-chrome-launcher",
+			"karma-coffee-preprocessor",
+			"karma-coverage",
+			"karma-firefox-launcher",
+			"karma-html2js-preprocessor",
+			"karma-jade-preprocessor",
+			"karma-jasmine",
+			"karma-ng-html2js-preprocessor",
+			"karma-ng-jade2js-preprocessor",
+			"karma-ng-scenario",
+			"karma-phantomjs-launcher",
+			"karma-requirejs",
+			"karma-script-launcher"
+		],
+
 		// list of files / patterns to load in the browser
 		files: [
-      'client/bower_components/jquery/dist/jquery.js',
-      'client/bower_components/angular/angular.js',
-      'client/bower_components/angular-mocks/angular-mocks.js',
-      'client/bower_components/angular-resource/angular-resource.js',
-      'client/bower_components/angular-cookies/angular-cookies.js',
-      'client/bower_components/angular-sanitize/angular-sanitize.js',
-      'client/bower_components/angular-route/angular-route.js',
-      'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'client/bower_components/lodash/dist/lodash.compat.js',
-      'client/bower_components/angular-socket-io/socket.js',
-      'client/bower_components/angular-ui-router/release/angular-ui-router.js',
-		'client/bower_components/ngstorage/ngStorage.js',
-      'client/app/app.js',
-      'client/app/app.coffee',
-      'client/app/**/*.js',
-      'client/app/**/*.coffee',
-      'client/components/**/*.js',
-      'client/components/**/*.coffee',
-      'client/app/**/*.jade',
-      'client/components/**/*.jade',
-      'client/app/**/*.html',
-      'client/components/**/*.html'
-    ],
+		  	'client/bower_components/jquery/dist/jquery.js',
+		  	'client/bower_components/angular/angular.js',
+		  	'client/bower_components/angular-mocks/angular-mocks.js',
+		  	'client/bower_components/angular-resource/angular-resource.js',
+		  	'client/bower_components/angular-cookies/angular-cookies.js',
+		  	'client/bower_components/angular-sanitize/angular-sanitize.js',
+		  	'client/bower_components/angular-route/angular-route.js',
+		  	'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+		  	'client/bower_components/lodash/dist/lodash.compat.js',
+		  	'client/bower_components/angular-socket-io/socket.js',
+		  	'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+			'client/bower_components/ngstorage/ngStorage.js',
+			'client/app/app.js',
+			'client/app/app.coffee',
+			'client/app/**/*.js',
+			'client/app/**/*.coffee',
+			'client/components/**/*.js',
+			'client/components/**/*.coffee',
+			'client/app/**/*.jade',
+		  	'client/components/**/*.jade',
+		  	'client/app/**/*.html',
+		  	'client/components/**/*.html'
+    	],
+		reporters: ['progress', 'coverage'],
 
 		preprocessors: {
 			'**/*.jade': 'ng-jade2js',
 			'**/*.html': 'html2js',
-			'client/app/**/*.js': 'babel',
+			'client/app/**/*.js': ['babel', 'coverage'],
 			'**/*.coffee': 'coffee',
+		},
+
+		coverageReporter: {
+			type: 'html',
+			dir: 'coverage/'
 		},
 
 		ngHtml2JsPreprocessor: {
