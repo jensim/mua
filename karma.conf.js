b296e7f727db81b4ecb9de3c738caa02b7551192
jensim <jens.brimberg@gmail.com>
2015-11-09 16:09:08 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -110,7 +110,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS', 'Chrome', 'Firefox', 'Opera'],
+    browsers: ['PhantomJS', 'Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
